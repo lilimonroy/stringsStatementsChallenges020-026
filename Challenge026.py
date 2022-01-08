d3e15b424e244ed21a55ffb3b6042a0198ac5d27
Lili Monroy <galadriel@comunidad.unam.mx>
2022-01-07 18:36:24 -0600

    UpdateChallenge26

--- Challenge026.py
+++ Challenge026.py
@@ -1,3 +1,4 @@
+#----------* CHALLENGE 26 *----------
 #Pig Latin takes the first consonant of a word, moves it to the end of the word and adds on an “ay”. If a word begins with a vowel you just add
 # “way” to the end. For example, pig becomes igpay, banana becomes ananabay, and aadvark becomes aadvarkway. Create a program that will ask the
 #user to enter a word and change it into Pig Latin. Make sure the new word is displayed in lower case.
@@ -8,7 +9,7 @@ lenghWord = len(word)
 word.lower()
 firstLetter = word[0]
 
-if firstLetter == "a" and "e" and "i" and "o" and "u":
-    print(word+"ay")
+if firstLetter == ("a" or "e" or "i" or "o" or "u"): #It's important to use the parenthesis because you wnat considerer every 'or' and everithimg is a whole condition. Predomina el '==' ante el 'or'.
+    print(word+"way")
 else:
     print(word[1:lenghWord+1]+word[0]+"ay")
